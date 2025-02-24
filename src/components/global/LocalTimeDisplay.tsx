343be0a45b0412ab208701992848709d22b123c8
vara-alta <admin@vara-alta.com>
2025-02-24 17:16:49 -0600

    Mobile version top position updated

--- src/components/global/LocalTimeDisplay.tsx
+++ src/components/global/LocalTimeDisplay.tsx
@@ -26,7 +26,7 @@ export default function LocalTimeDisplay() {
   }, []);
 
   return (
-    <div className="fixed text-white font-ubuntu font-semibold bg-[#afe6d8] bg-gradient-to-r from-[#afe6d8] to-[#f93a97] px-2 py-1 rounded-3xl tracking-wide md:bottom-4 left-4 z-10">
+    <div className="fixed text-white font-ubuntu font-semibold bg-[#afe6d8] bg-gradient-to-r from-[#afe6d8] to-[#f93a97] px-2 py-1 rounded-3xl tracking-wide bottom-14 md:bottom-4 left-4 z-10">
       {localTime}
     </div>
   );
