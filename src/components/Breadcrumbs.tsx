eac1947292b7956d64087e91663840fb8df148d3
vara-alta <admin@vara-alta.com>
2025-02-26 10:50:19 -0600

    Dynamically breadcrumb length adjustability

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -2,10 +2,35 @@
 
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Breadcrumbs() {
   const pathname = usePathname();
   const pathnames = pathname.split("/").filter((x) => x);
+  const [breadcrumbWordLimit, setbreadcrumbWordLimit] = useState(8);
+
+  useEffect(() => {
+    const updateWordLimit = () => {
+      setbreadcrumbWordLimit(
+        window.innerWidth < 448
+          ? 4
+          : window.innerWidth < 576
+          ? 6
+          : window.innerWidth < 704
+          ? 8
+          : window.innerWidth < 823
+          ? 10
+          : 12
+      );
+    };
+
+    updateWordLimit();
+    window.addEventListener("resize", updateWordLimit);
+
+    return () => {
+      window.removeEventListener("resize", updateWordLimit);
+    };
+  }, []);
 
   return (
     <nav aria-label="breadcrumb">
@@ -21,21 +46,17 @@ export default function Breadcrumbs() {
         {pathnames.map((value, index) => {
           const href = `/${pathnames.slice(0, index + 1).join("/")}`;
           const isLast = index === pathnames.length - 1;
+          const words = value.replace(/-/g, " ").split(" ");
+          const truncatedValue = words.slice(0, breadcrumbWordLimit).join(" ");
+          const isTruncated = words.length > breadcrumbWordLimit;
 
           return (
             <li key={href} className="flex items-center">
               <span className="mx-2">/</span>
               {isLast ? (
                 <span>
-                  {
-                    value
-                      .replace(/-/g, " ") // replace all hyphens with spaces
-                      .split(" ") // split the string into an array of words
-                      .slice(0, 8) // get the first 8 words
-                      .join(" ") // join the first 8 words with a space
-                  }
-                  {value.split(" ").length > 8 ? "..." : ""}{" "}
-                  {/* Add "..." if truncated */}
+                  {truncatedValue}
+                  {isTruncated ? "..." : ""}
                 </span>
               ) : (
                 <Link
